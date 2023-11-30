1491ce16796c489f4a8e4d92248475d83c863806
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2023-11-30 23:16:29 +0100

    update script

--- update.sh
+++ update.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-sudo echo ''
-echo '-------------------------'
-echo 'UPDATING PACKAGE INDEX'
-echo '-------------------------'
-echo ''
+print_header() {
+  echo -e "\n\e[1;32m$1\e[0m"
+}
+
+# Function to update packages
+update_packages() {
+  print_header "UPDATING $1 PACKAGES"
+  sudo $2 $3
+}
+
+# Update APT package index and then upgrade the packages
+print_header "UPDATING APT PACKAGE INDEX"
 sudo apt update
-echo ''
-echo '-------------------------'
-echo 'UPGRADING PACKAGES'
-echo '-------------------------'
-echo ''
+
+print_header "UPGRADING APT PACKAGES"
 sudo apt upgrade -y
-echo ''
-echo '-------------------------'
-echo 'UPDATING SNAP PACKAGES'
-echo '-------------------------'
-echo ''
-sudo snap refresh &&
+
+# Update SNAP packages
+print_header "UPDATING SNAP PACKAGES"
+sudo snap refresh
+
+# Update FLATPAK packages
+print_header "UPDATING FLATPAK PACKAGES"
+flatpak update -y
+
 sleep 1
-echo ''
-echo '-------------------------'
-echo 'UPDATE COMPLETE'
-echo '-------------------------'
+
+print_header "* * * UPDATE COMPLETE * * *"
+
 sleep 1
