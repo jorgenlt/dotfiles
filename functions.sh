921f3fb1f40366e557633654fd0a77e1ab73165f
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-05-06 11:38:54 +0200

    update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Function to suspend the system
-suspend_system() {
-    local total_minutes=$1
-    local total_seconds=$((total_minutes * 60))
+# Function to display the countdown timer
+display_countdown() {
+    local total_seconds=$1
     local hours minutes seconds
 
     while [ $total_seconds -gt 0 ]; do
@@ -11,10 +10,19 @@ suspend_system() {
         ((minutes=($total_seconds%3600)/60))
         ((seconds=$total_seconds%60))
 
-        printf "System will sleep in %02d:%02d:%02d\r" $hours $minutes $seconds
+        printf "%02d:%02d:%02d\r" $hours $minutes $seconds
         sleep 1
         ((total_seconds--))
     done
+}
+
+# Function to suspend the system
+suspend_system() {
+    local total_minutes=$1
+    local total_seconds=$((total_minutes * 60))
+
+    echo "System will sleep in:"
+    display_countdown $total_seconds
 
     sudo systemctl suspend
 }
@@ -23,17 +31,10 @@ suspend_system() {
 shutdown_system() {
     local total_minutes=$1
     local total_seconds=$((total_minutes * 60))
-    local hours minutes seconds
 
-    while [ $total_seconds -gt 0 ]; do
-        ((hours=$total_seconds/3600))
-        ((minutes=($total_seconds%3600)/60))
-        ((seconds=$total_seconds%60))
-
-        printf "System will shut down in %02d:%02d:%02d\r" $hours $minutes $seconds
-        sleep 1
-        ((total_seconds--))
-    done
+    echo "System will shut down in:"
+    display_countdown $total_seconds
 
     sudo shutdown now
 }
+
