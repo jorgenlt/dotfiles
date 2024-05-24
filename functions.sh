e523ef15ea8f18abdde5c9441cfd94896f08a425
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-05-24 11:43:49 +0200

    Function to cat files

--- functions.sh
+++ functions.sh
@@ -2,49 +2,67 @@
 
 # Function to display the countdown timer
 display_countdown() {
-    local total_seconds=$1
-    local hours minutes seconds
+  local total_seconds=$1
+  local hours minutes seconds
 
-    while [ $total_seconds -gt 0 ]; do
-        ((hours=$total_seconds/3600))
-        ((minutes=($total_seconds%3600)/60))
-        ((seconds=$total_seconds%60))
+  while [ $total_seconds -gt 0 ]; do
+    ((hours = $total_seconds / 3600))
+    ((minutes = ($total_seconds % 3600) / 60))
+    ((seconds = $total_seconds % 60))
 
-        printf "%02d:%02d:%02d\r" $hours $minutes $seconds
-        sleep 1
-        ((total_seconds--))
-    done
+    printf "%02d:%02d:%02d\r" $hours $minutes $seconds
+    sleep 1
+    ((total_seconds--))
+  done
 }
 
 # Function to suspend the system
 suspend_system() {
-    local total_minutes=$1
-    local total_seconds=$((total_minutes * 60))
+  local total_minutes=$1
+  local total_seconds=$((total_minutes * 60))
 
-    echo "System will sleep in:"
-    display_countdown $total_seconds
+  echo "System will sleep in:"
+  display_countdown $total_seconds
 
-    sudo systemctl suspend
+  sudo systemctl suspend
 }
 
 # Function to shut down the system
 shutdown_system() {
-    local total_minutes=$1
-    local total_seconds=$((total_minutes * 60))
+  local total_minutes=$1
+  local total_seconds=$((total_minutes * 60))
 
-    echo "System will shut down in:"
-    display_countdown $total_seconds
+  echo "System will shut down in:"
+  display_countdown $total_seconds
 
-    sudo shutdown now
+  sudo shutdown now
 }
 
 # Function to get the CPU temperature
 cpu_temp() {
-    sensors k10temp-pci-00c3 | grep Tctl | awk '{print $2}'
+  sensors k10temp-pci-00c3 | grep Tctl | awk '{print $2}'
 }
 
-# Function to cat pdf-files
-catpdf() {
-    pdftotext "$1" - | cat
+# Function to cat files
+catfile() {
+  case "$1" in
+  *.pdf)
+    pdftotext "$1" -
+    ;;
+  *.ods | *.odt)
+    odt2txt "$1"
+    ;;
+  *.docx)
+    temp_file=$(mktemp)
+    docx2txt "$1" "$temp_file"
+    cat "$temp_file"
+    rm "$temp_file"
+    ;;
+  *.xlsx)
+    xlsx2csv "$1" | sed 's/,/\t/g'
+    ;;
+  *)
+    cat "$1"
+    ;;
+  esac
 }
-
