521946f5c55358402891c6f8c4b2616b9bb35247
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-06-02 17:32:56 +0200

    update install script

--- remapper/create-symlink-laptop.sh
+++ remapper/create-symlink-laptop.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# Create symlink
-ln -sv "$DOT/remapper/jlt-colemak-custom.json" "$HOME/.config/input-remapper-2/presets/AT Translated Set 2 keyboard"
+SOURCE_FILE="$DOT/remapper/jlt-colemak-custom.json"
+SYMLINK_PATH="$HOME/.config/input-remapper-2/presets/AT Translated Set 2 keyboard/jlt-colemak-custom.json"
+
+# Check if the symlink already exists
+if [ ! -L "$SYMLINK_PATH" ]; then
+  # Create the symlink
+  ln -sv "$SOURCE_FILE" "$SYMLINK_PATH"
+else
+  echo "Symlink already exists."
+fi
