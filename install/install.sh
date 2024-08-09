118d69458067c7c1dfe613709e4fc8d6c9db6590
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-08-09 20:35:15 +0200

    update aliases.sh

--- install/install.sh
+++ install/install.sh
@@ -69,9 +69,9 @@ if ! command -v gh &>/dev/null; then
   echo
   (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y))
   sudo mkdir -p -m 755 /etc/apt/keyrings
-  wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+  wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
   sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
   sudo apt update
   sudo apt install gh -y
 else
@@ -116,7 +116,8 @@ echo
 if ! command -v rclone &>/dev/null; then
   echo "Rclone is not installed. Starting installation..."
   echo
-  sudo -v ; curl https://rclone.org/install.sh | sudo bash
+  sudo -v
+  curl https://rclone.org/install.sh | sudo bash
 else
   echo "Rclone ✅"
 fi
