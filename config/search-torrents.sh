27234446cab7bc8bc5a3b2b3cef57dafbda8fddb
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-12-26 12:29:26 +0800

    update search-torrents.sh

--- config/search-torrents.sh
+++ config/search-torrents.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <argument>"
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <argument1> <argument2> ... <argumentN>"
   exit 1
 fi
 
-search=$1
+s="$*"
 
 websites=(
-  "https://1337x.to/search/${search}/1/"
-  "https://tprbay.xyz/search/${search}"
-  "https://cloudtorrents.com/search?query=${search}"
-  "https://www.torrentdownload.info/search?q=${search}"
+  "https://1337x.to/search/${s}/1/"
+  "https://tprbay.xyz/search/${s}"
+  "https://cloudtorrents.com/search?query=${s}"
+  "https://www.torrentdownload.info/search?q=${s}"
+  "https://torrentgalaxy.to/torrents.php?search=${s}"
+  "https://www.limetorrents.lol/search/all/${s}"
 )
 
 for url in "${websites[@]}"; do
-  xdg-open "$url" &
+  vivaldi --incognito "$url" &
 done
