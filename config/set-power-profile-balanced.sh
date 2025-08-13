44475e521fd9ccf44e4436eb410cb3b21b0456b7
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-08-13 10:10:54 +0200

    power profile scripts

---
+++ config/set-power-profile-balanced.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+system76-power profile balanced
+
+notify-send "Power settings" "Power profile set to Balanced"
---
+++ config/set-power-profile-performance.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+system76-power profile performance
+
+notify-send "Power settings" "Power profile set to High Performance"
