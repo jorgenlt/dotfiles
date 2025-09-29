200af7775ba30a409e91a958e86bf8fb5032cdd8
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-09-29 10:52:53 +0200

    update install.sh

--- install/install.sh
+++ install/install.sh
@@ -194,7 +194,7 @@ echo
 
 print_header "Installing apt packages"
 sudo apt update
-sudo apt install -y btop htop tree lm-sensors libnotify-bin vlc gnome-tweaks make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt install -y btop htop tree lm-sensors libnotify-bin vlc gnome-tweaks xclip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 # Create symlinks in home folder
 create_symlink_home ".gitconfig"
