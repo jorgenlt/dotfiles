ac9793ba8f12b4bb42df2ca3fecd217b4b5c20d5
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2026-04-25 10:44:20 +0700

    startup script

---
+++ config/startup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+vivaldi &
+vivaldi --app-id=eigpmdhekjlgjgcppnanaanbdmnlnagl &   # outlook
+gnome-terminal &
+diodon &
+/home/jlt/.joplin/Joplin.AppImage &
+/home/jlt/dotfiles/config/rclone-onedrive.sh &
