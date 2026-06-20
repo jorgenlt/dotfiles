ed77c6f4524ac6798f5e6039f72ad6a42bf68152
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2026-06-20 17:08:24 +0200

    daysuntil.sh

---
+++ config/daysuntil.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo "Usage: $0 YYYY-MM-DD"
+  exit 1
+fi
+
+target="$1"
+days=$(( ($(date -d "$target" +%s) - $(date +%s)) / 86400 ))
+
+if [ "$days" -gt 0 ]; then
+  echo "$days days left until $target"
+elif [ "$days" -eq 0 ]; then
+  echo "Today is $target!"
+else
+  echo "$((-days)) days ago was $target"
+fi
+
+# Usage: ./daysleft.sh 2026-12-25
