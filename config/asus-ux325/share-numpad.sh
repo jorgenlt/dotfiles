d15cc3691b24d98d30f3b95dd0da296e525a6a37
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2026-09-13 11:40:38 +0200

    asus-ux325 share button numpad touch

---
+++ config/asus-ux325/share-numpad.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export DISPLAY=:0
+export XAUTHORITY=/home/jlt/.Xauthority
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+export XDG_RUNTIME_DIR=/run/user/1000
+
+notify-send -i pop-os "share-numpad.sh" "in home folder"
+
+echo "share-numpad.sh"
+
